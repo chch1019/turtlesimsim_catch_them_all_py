c78fe7e2a497e1761d9c5edcf2fc011b613322e3
chaitanya krishna Chirumamilla <ch.chaitanya94@gmail.com>
2022-10-12 02:13:42 +0200

    utilized custon generated interfaces

--- turtlesimsim_catch_them_all_py/turtle_controller.py
+++ turtlesimsim_catch_them_all_py/turtle_controller.py
@@ -5,30 +5,41 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from my_robot_interfaces.msg import Turtle
+from my_robot_interfaces.msg import TurtleArray
 
 
 class TurtleControllerNode(Node):  # MODIFY NAME
     def __init__(self):
         super().__init__("turtle_controller")  # MODIFY NAME
-        self.x_target = 4.0
-        self.y_target = 8.0
+        self.turtle_to_catch = None
+        #self.x_target = 4.0
+        #self.y_target = 8.0
         
         self.pose_ = None
         self.pose_subscriber_ = self.create_subscription(
             Pose, "turtle1/pose", self.callback_turtle_pose, 10)
+        
+        self.alive_turtle_subscriber_ = self.create_subscription(
+            TurtleArray, "alive_turtles", self.callback_alive_turtles, 10)
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
 
         self.control_loo_timer_ = self.create_timer(0.01, self.control_loop)
 
     def callback_turtle_pose(self, msg):
         self.pose_ = msg
+    
+    def callback_alive_turtles(self, msg):
+        if len(msg.turtles)>0:
+            self.turtle_to_catch = msg.turtles[0]
+        
 
     def control_loop(self):
-        if self.pose_ == None:
+        if self.pose_ == None or self.turtle_to_catch == None:
             return
         
-        x_dist = self.x_target - self.pose_.x
-        y_dist = self.y_target - self.pose_.y
+        x_dist = self.turtle_to_catch.x - self.pose_.x
+        y_dist = self.turtle_to_catch.y - self.pose_.y
         target_dist = math.sqrt(x_dist * x_dist + y_dist * y_dist)
 
         msg = Twist()
